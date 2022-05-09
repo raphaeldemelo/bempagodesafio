bb5e0c3c981d413cdb7f6dff0302df3b13ecc24f
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-05-09 10:30:55 -0300

    finalizando created

--- src/Components/Created/index.js
+++ src/Components/Created/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 import Perfil from '../../assets/perfil.jpg';
-import {
-  Container,
-  Detalhes,
-  AreaImagem,
-  Imagem,
-  Texto,
-  Titulo,
-  AreaTexto
-} from './styles';
-
 import './styles.css';
 
 export default function Created() {
@@ -23,7 +13,7 @@ export default function Created() {
 
         <div className="text">
           <p>Criado por</p>
-          <p>@raphaeldemelo</p>
+          <strong>@raphaeldemelo</strong>
         </div>
 
       </div>
